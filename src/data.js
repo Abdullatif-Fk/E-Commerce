3b29cb58b1cb447203255073843cb8e2ae66bd4c
Abdullatif-Fk <abdul647.fk@gmail.com>
2022-01-10 00:42:34 +0200

    finished home page slider

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://media.glamour.com/photos/569592ead9dab9ff41b2a68a/master/pass/fashion-2015-08-reformation-big-bust-collection-white-crop-top-main.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "CDC1B7",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/XsdmR2c/1.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
