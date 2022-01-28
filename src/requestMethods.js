5c69c0119a6291a436b7a734daa771d32ee7d5ef
Abdullatif-Fk <abdul647.fk@gmail.com>
2022-01-28 22:03:09 +0200

    fixing some code

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTBhYzJiOGQ3OWU1NjcwODQyOGZhZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MjYwMTgwNywiZXhwIjoxNjQyODYxMDA3fQ.hfuDENWs8UBTMPUo2-X74AONM6YFu7U5j1U-r5jcQfw";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+  .currentUser?.accessToken;
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
